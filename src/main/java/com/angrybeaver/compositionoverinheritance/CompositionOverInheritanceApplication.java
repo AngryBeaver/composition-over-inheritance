eeebd0e82e60df3a9950b0f72a45108afe26ba43
AngryBeaver <AngryBeaver@verplanmich.net>
2018-12-12 13:24:29 +0100

    initial Commit

---
+++ src/main/java/com/angrybeaver/compositionoverinheritance/CompositionOverInheritanceApplication.java
@@ -0,0 +1,12 @@
+package com.angrybeaver.compositionoverinheritance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositionOverInheritanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositionOverInheritanceApplication.class, args);
+	}
+}
